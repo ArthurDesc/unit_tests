d7a1e5e2df975cf7aaad4ca04ca79df7ab643c34
ArthurDesc <arthur.descourvieres@gmail.com>
2025-04-22 16:42:04 +0200

    started second job test

---
+++ job2/src/Math.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+class Math
+{
+    public function addition($a, $b)
+    {
+        return $a + $b;
+    }
+} 
---
+++ job2/tests/MathTest.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Math;
+
+class MathTest extends TestCase
+{
+    public function testAddition()
+    {
+        $math = new Math();
+        $this->assertEquals(5, $math->addition(2, 3));
+    }
+} 
